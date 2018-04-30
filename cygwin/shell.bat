6e9d0ebc8446ae87dc3d08e9693a408074bc6b18
jccultima123 <johncyrillcorsanes@gmail.com>
2018-04-30 23:05:20 +0800

    Initial Commit

---
+++ cygwin/shell.bat
@@ -0,0 +1,13 @@
+@echo off
+REM Get the current directory 
+REM
+for /f "tokens=* delims=/" %%A in ('cd') do set CURRENT_DIR=%%A
+
+SET SHELLDIR=%CURRENT_DIR%
+SET SHELL=%SHELLDIR%/bash.exe
+SET BASH=%SHELL%
+
+
+SET PATH=%SHELLDIR%;%PATH%
+
+"%SHELL%" %*
